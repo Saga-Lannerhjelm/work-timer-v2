eb9fda86c6534503b984e131be697cf90f4d4a94
Saga-Lannerhjelm <saga.lannerhjelm@icloud.com>
2025-10-05 15:50:14 +0200

    Make notes input into text area

--- src/components/NotesInput.tsx
+++ src/components/NotesInput.tsx
@@ -7,13 +7,20 @@ interface Props {
 
 const NotesInput: React.FC<Props> = ({ value, onChange }) => (
   <div>
-    <input
-      type="text"
-      placeholder="Note"
-      value={value}
+    <label
+      htmlFor="message"
+      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+    >
+      Add notes to the started activity
+    </label>
+    <textarea
+      id="message"
+      className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      placeholder="Add note to event..."
       onChange={(e) => onChange(e.target.value)}
-      className="border p-2 rounded w-full"
-    />
+      value={value}
+      rows={5}
+    ></textarea>
   </div>
 );
 
